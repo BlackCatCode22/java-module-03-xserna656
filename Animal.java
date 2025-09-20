033b64ae08aa7593fe259a27fe586600eb2577b5
xserna656 <xserna2468@gmail.com>
2025-09-20 13:41:26 -0700

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,25 @@
+public class Animal {
+
+    int age;
+    String name;
+
+    public Animal() {}
+
+    public Animal(int age, String name){
+        this.age = age;
+        this.name = name;
+    }
+
+    public void makeNoise() {
+        System.out.println("Hello, I am an animal");
+    }
+
+    public void eat() {
+        System.out.println("Munch Munch");
+    }
+
+    public void doSomethingPrivate() {
+        System.out.println("Hey this method is private");
+
+    }
+}
---
+++ SuperDuper.java
@@ -0,0 +1,9 @@
+ public class SuperDuper {
+
+     public static void main(String[] args) {
+
+        Cat myCat = new Cat(3, "George", "Purina One");
+        System.out.println(myCat.age + " " + myCat.name + " " + myCat.catFoodPreference);
+
+    }
+}
